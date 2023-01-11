714a233c6061290891c4d7bbe8a44fc0eaccec9d
VictoKu1 <kushnir.victor@gmail.com>
2023-01-11 22:34:06 +0200

    .

--- app.py
+++ app.py
@@ -4,33 +4,41 @@ import networkx as nx
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/match_and_merge', methods=['POST'])
+@app.route('/result', methods=['GET', 'POST'])
 def match_and_merge_route():
     # Get the form data
-    graph = request.json.get('graph')
-    number = request.json.get('number')
-    edges = eval(graph)
-    G = nx.Graph()
-    G.add_edges_from(edges)
-    k = int(number)
-    # Call match_and_merge function
-    result = saapir.match_and_merge(G, k)
-    session['result'] = result
-    return redirect(url_for('result'))
-
-@app.route('/result', methods=['GET'])
-def result():
-    # Get the result from the session
-    result = session.get('result')
-
-    # Render the result page
-    return render_template('result.html', result=result)
+    if request.method == 'POST':
+        graph = request.form['graph']
+        number = request.form['number']
+        # graph = request.json.get('graph')
+        # print graph type
+        print(type(graph))
+        # number = request.json.get('number')
+        edges = eval(graph)
+        G = nx.Graph()
+        G.add_edges_from(edges)
+        k = int(number)
+        # Call match_and_merge function
+        result = saapir.match_and_merge(G, k)
+        print(result)
+        result = str(result)
+        print(type(result))
+        # Load /result.html with the result
+        return render_template('result.html', result=result)
+    else:
+        return render_template('result.html')
 
 if __name__ == '__main__':
     # Run the app
     app.run(host="0.0.0.0", port=5001)
+
+
+
+
+
+
 
