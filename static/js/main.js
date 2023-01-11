af5ba6ed4cef75c94fc87ac4839b06dfe15a53fa
VictoKu1 <kushnir.victor@gmail.com>
2023-01-11 09:23:56 +0200

    .

--- static/js/main.js
+++ static/js/main.js
@@ -2,6 +2,9 @@ document.getElementById("form").addEventListener("submit", function(event) {
     event.preventDefault();
     var graph = document.getElementById("graph").value;
     var number = document.getElementById("number").value;
+    //* Print the values to the console
+    console.log(graph);
+    console.log(number);
     var data = { graph: graph, number: number };
     fetch("/run_algorithm", {
             method: "POST",
