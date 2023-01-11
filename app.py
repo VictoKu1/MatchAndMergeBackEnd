119947d089610c671e6369f33fcee48878710aa5
VictoKu1 <kushnir.victor@gmail.com>
2023-01-11 09:10:55 +0200

    .

--- app.py
+++ app.py
@@ -1,25 +1,29 @@
 from flask import Flask, request, jsonify, render_template
-import networkx as nx
 import social_aware_assignment_of_passengers_in_ridesharing as saapir
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
-def index():
-    return render_template("index.html")
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
 
-@app.route("/match_and_merge", methods=["POST"])
-def match_and_merge():
-    # Get the graph and number from the request body
-    data = request.json
-    graph = data['graph']
-    number = data['number']
-    # Convert the graph from json format to networkx format
-    G = nx.Graph(graph)
-    # Call the match_and_merge function
-    partition = saapir.match_and_merge(G, number)
-    # Return the partition as a JSON response
-    return jsonify(partition)
+@app.route('/match_and_merge', methods=['POST'])
+def match_and_merge_route():
+    # Get the form data
+    graph = request.json.get('graph')
+    number = request.json.get('number')
 
-if __name__ == "__main__":
-    app.run()
+    # Convert input graph data to edges list format
+    edges = [tuple(map(int,edge.split(','))) for edge in graph.strip('[]').split('), (')]
+    G=nx.Graph()
+    G.add_edges_from(edges)
+    k = int(number)
+    # Call match_and_merge function
+    result = saapir.match_and_merge(G, k)
+    return jsonify({'result': result})
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+    
