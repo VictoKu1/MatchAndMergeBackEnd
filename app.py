00007618d1a5a9f6323f5b620688b1518c790ec2
VictoKu1 <kushnir.victor@gmail.com>
2023-01-11 11:03:59 +0200

    .

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 import social_aware_assignment_of_passengers_in_ridesharing as saapir
 import networkx as nx
-
+import socket
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
@@ -23,13 +23,12 @@ def match_and_merge_route():
 
 if __name__ == '__main__':
     # Find a free port and print it
-    import socket
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind(('', 0))
+    # Put the port number to the variable p
+    p = s.getsockname()[1]
     print(s.getsockname()[1])
     s.close()
     # Run the app
-    app.run(host="0.0.0.0", port=80)
-
-
+    app.run(host="0.0.0.0", port=p)
 
