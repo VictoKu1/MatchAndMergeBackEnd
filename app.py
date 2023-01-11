e059ba89fed43e42667de9ff3c58e49d473c2214
VictoKu1 <kushnir.victor@gmail.com>
2023-01-11 23:00:59 +0200

    .

--- app.py
+++ app.py
@@ -11,24 +11,18 @@ def home():
 @app.route('/result', methods=['GET', 'POST'])
 def match_and_merge_route():
     # Get the form data
-    if request.method == 'POST':
-        graph = request.form['graph']
-        number = request.form['number']
-        edges = eval(graph)
-        G = nx.Graph()
-        G.add_edges_from(edges)
-        k = int(number)
-        # Call match_and_merge function
-        result = saapir.match_and_merge(G, k)
-        return render_template('result.html', result=result)
-    else:
-        return render_template('result.html')
+    graph = request.form['graph']
+    number = request.form['number']
+    edges = eval(graph)
+    G = nx.Graph()
+    G.add_edges_from(edges)
+    k = int(number)
+    # Call match_and_merge function
+    result = saapir.match_and_merge(G, k)
+    return render_template('result.html', result=result)
 
 if __name__ == '__main__':
     # Run the app
     app.run(host="0.0.0.0", port=5001)
-
-
-
 
 
